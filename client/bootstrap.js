8c9504556b63258c7a4bf03d00081dd661b96722
leemyunggyu <jaylee@hatiolab.com>
2019-07-24 16:42:42 +0900

    Update bootstrap.js

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,1 +1,40 @@
-export default function bootstrap() {}
+import { registMenuProvider } from '@things-factory/menu-base'
+import { store, client, gqlBuilder } from '@things-factory/shell'
+import gql from 'graphql-tag'
+
+export default function bootstrap() {
+  store.dispatch(
+    registMenuProvider(async () => {
+      const response = await client.query({
+        query: gql`
+        query {
+          menus(${gqlBuilder.buildArgs({
+            filters: [
+              {
+                name: 'category',
+                operator: 'like',
+                value: 'client'
+              }
+            ]
+          })}) {
+            items {
+              id
+              name
+              childrens {
+                id
+                name
+                routingType
+                idField
+                resourceName
+                template
+              }
+            }
+          }
+        }
+      `
+      })
+
+      return response.data.menus.items
+    })
+  )
+}
