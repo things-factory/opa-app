d445bae72a5855127bc555f69ff84b09dbb3ff44
hoppaang <hoppaang@hatiolab.com>
2019-07-25 11:07:09 +0900

    form style - company profile

--- client/pages/company-profile.js
+++ client/pages/company-profile.js
@@ -1,5 +1,5 @@
 import { i18next, localize } from '@things-factory/i18n-base'
-import { PageView, ScrollbarStyles } from '@things-factory/shell'
+import { PageView } from '@things-factory/shell'
 import { css, html } from 'lit-element'
 
 class CompanyProfile extends localize(i18next)(PageView) {
@@ -25,21 +25,25 @@ class CompanyProfile extends localize(i18next)(PageView) {
 
   static get styles() {
     return [
-      ScrollbarStyles,
       css``,
       css`
         :host {
+          overflow: auto;
+        }
+
+        form {
+          display: grid;
+          grid-template-columns: repeat(12, 1fr);
+          grid-gap: 12px 5px;
+          grid-auto-rows: minmax(24px, auto);
+          max-width: var(--form-max-width);
+          margin: var(--form-margin);
         }
         fieldset {
-          border: var(--fieldset-border);
-          max-width: var(--fieldset-max-width);
-          margin: var(--fieldset-margin);
+          display: contents;
         }
-        fieldset div {
-          padding: var(--fieldset-div-padding);
-        }
-        fieldset legend {
-          margin: auto;
+        legend {
+          grid-column: span 12;
           text-transform: capitalize;
 
           padding: var(--legend-padding);
@@ -47,70 +51,117 @@ class CompanyProfile extends localize(i18next)(PageView) {
           color: var(--legend-text-color);
           border-bottom: var(--legend-border-bottom);
         }
-        fieldset label {
-          display: inline-block;
-          width: 30%;
-          text-align: right;
 
-          padding: var(--label-padding);
+        fieldset > label {
+          grid-column: span 3;
+          text-align: right;
+          text-transform: capitalize;
+
           color: var(--label-color);
           font: var(--label-font);
         }
-        fieldset input {
-          min-width: 35%;
-          min-height: 25px;
+
+        fieldset > input,
+        fieldset > table,
+        fieldset > select,
+        fieldset > textarea {
+          grid-column: span 8;
 
           border: var(--input-field-border);
           border-radius: var(--input-field-border-radius);
           padding: var(--input-field-padding);
           font: var(--input-field-font);
         }
+
+        fieldset > input[type='checkbox'],
+        fieldset > input[type='radio'] {
+          justify-self: end;
+          align-self: start;
+          grid-column: span 3 / auto;
+        }
+
+        fieldset > input[type='checkbox'] + label,
+        fieldset > input[type='radio'] + label {
+          text-align: left;
+          grid-column: span 9 / auto;
+
+          font: var(--form-sublabel-font);
+          color: var(--form-sublabel-color);
+        }
+
         input:focus {
           outline: none;
           border: 1px solid var(--focus-background-color);
         }
+
+        .profile-brand {
+          background: url('../assets/images/brand.png') center center no-repeat;
+          width: var(--profile-brand-width);
+          height: var(--profile-brand-height);
+          margin: var(--profile-brand-padding);
+        }
+
+        @media screen and (max-width: 400px) {
+          form{
+            max-width:90%;
+            grid-gap: 5px;
+          }
+          fieldset > label {
+            grid-column: span 12;
+            text-align: left;
+            align-self: end
+          }
+          fieldset > input,
+          fieldset > table,
+          fieldset > select,
+          fieldset > textarea {
+            grid-column: span 12;
+          }
+          fieldset > input[type='checkbox'],
+          fieldset > input[type='radio'] {
+            justify-self: start;
+            align-self: center;
+            grid-column: span 1 / auto;
+          }
+
+          fieldset > input[type='checkbox'] + label,
+          fieldset > input[type='radio'] + label {
+            grid-column: span 11 / auto;
+            align-self: center
+          }
       `
     ]
   }
 
   render() {
     return html`
+      <div class="profile-brand"></div>
+
       <form>
         <fieldset>
           <legend>${i18next.t('title.information')}</legend>
-          <div>
-            <label>${i18next.t('label.company_name')}</label>
-            <input name="company_name" />
-          </div>
+          <label>${i18next.t('label.company_name')}</label>
+          <input name="company_name" />
 
-          <div>
-            <label>${i18next.t('label.company_name')}</label>
-            <input name="default_delivery" />
-          </div>
+          <label>${i18next.t('label.company_name')}</label>
+          <input name="default_delivery" />
 
-          <div>
-            <label>${i18next.t('label.company_name')}</label>
-            <input name="default_contact_no" />
-          </div>
+          <label>${i18next.t('label.company_name')}</label>
+          <input name="default_contact_no" />
         </fieldset>
 
         <fieldset>
           <legend>${i18next.t('title.contact_point')}</legend>
 
-          <div>
-            <label>${i18next.t('label.name')}</label>
-            <input name="name" />
-          </div>
+          <label>${i18next.t('label.name')}</label>
+          <input name="name" />
 
-          <div>
-            <label>${i18next.t('label.email')}</label>
-            <input name="email" />
-          </div>
+          <label>${i18next.t('label.email')}</label>
+          <input name="email" />
+          <input type="checkbox" name="email" checked /> <label>I agree to receive event mail.</label>
 
-          <div>
-            <label>${i18next.t('label.phone_no')}</label>
-            <input name="phone_no" />
-          </div>
+          <label>${i18next.t('label.phone_no')}</label>
+          <input name="phone_no" />
         </fieldset>
       </form>
     `
