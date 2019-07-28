5c7666f76ac5df79003af9a91fbb4d5064fb9544
Hearty Oh <heartyoh@hatiolab.com>
2019-07-28 16:32:39 +0900

    v0.0.34

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -8,7 +8,7 @@ export default function bootstrap() {
    * things-board 기능을 메뉴에서 지원하기 위해서, VIEWER, PLAYER routing type을 추가함.
    */
   store.dispatch(addRoutingType('VIEWER', 'board-viewer', 'bar_chart'))
-  store.dispatch(addRoutingType('PLAYER', 'board-player', 'playlist_play'))
+  store.dispatch(addRoutingType('PLAYER', 'board-player', 'play_arrow'))
 
   /*
    * server endpoint를 설정할 수 있음. (기본은 client가 패포된 서버를 사용함.)
