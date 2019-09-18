b1ae87b980a3059cfcd679aab0144afbd6c46ba1
leemyunggyu <jaylee@hatiolab.com>
2019-09-18 14:37:28 +0800

    worksheet 작업중

--- client/pages/master/contact-point-list.js
+++ client/pages/master/contact-point-list.js
@@ -83,28 +83,28 @@ export class ContactPointList extends localize(i18next)(LitElement) {
     `
   }
 
-  get context() {
-    return {
-      title: i18next.t('title.contact_point'),
-      actions: [
-        {
-          title: i18next.t('button.save'),
-          action: this._saveContactPoints.bind(this)
-        },
-        {
-          title: i18next.t('button.delete'),
-          action: this._deleteContactPoints.bind(this)
-        }
-      ],
-      exportable: {
-        name: i18next.t('title.contact_point'),
-        data: this._exportableData.bind(this)
-      },
-      importable: {
-        handler: this._importableData.bind(this)
-      }
-    }
-  }
+  // get context() {
+  //   return {
+  //     title: i18next.t('title.contact_point'),
+  //     actions: [
+  //       {
+  //         title: i18next.t('button.save'),
+  //         action: this._saveContactPoints.bind(this)
+  //       },
+  //       {
+  //         title: i18next.t('button.delete'),
+  //         action: this._deleteContactPoints.bind(this)
+  //       }
+  //     ],
+  //     exportable: {
+  //       name: i18next.t('title.contact_point'),
+  //       data: this._exportableData.bind(this)
+  //     },
+  //     importable: {
+  //       handler: this._importableData.bind(this)
+  //     }
+  //   }
+  // }
 
   activated(active) {
     if (JSON.parse(active) && this.dataGrist) {
---
+++ client/pages/order/constants/location.js
@@ -0,0 +1,10 @@
+export const LOCATION_TYPE = {
+  BUFFER: {
+    name: 'buffer_location',
+    value: 'BUFFER'
+  },
+  SHELF: {
+    name: 'shelf_location',
+    value: 'SHELF'
+  }
+}
