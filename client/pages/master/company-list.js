d617f2f9ccb94300d7f87407422e083b82e9d0ca
Hearty Oh <heartyoh@hatiolab.com>
2019-09-20 15:06:24 +0800

    Test the sweetalert2 popup message at master data

--- client/pages/master/company-list.js
+++ client/pages/master/company-list.js
@@ -6,6 +6,7 @@ import gql from 'graphql-tag'
 import { css, html } from 'lit-element'
 import { openPopup } from '@things-factory/layout-base'
 import '../components/import-pop-up'
+import Swal from 'sweetalert2'
 
 class CompanyList extends localize(i18next)(PageView) {
   static get styles() {
@@ -354,41 +355,65 @@ class CompanyList extends localize(i18next)(PageView) {
       if (!response.errors) {
         this.dataGrist.fetch()
         document.dispatchEvent(
-          new CustomEvent('notify', {
-            detail: {
-              message: i18next.t('text.data_updated_successfully')
-            }
+          Swal.fire({
+            // position: 'top-end',
+            type: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
           })
+          // new CustomEvent('notify', {
+          //   detail: {
+          //     message: i18next.t('text.data_updated_successfully')
+          //   }
+          // })
         )
       }
     }
   }
 
   async _deleteCompanies() {
-    let confirmDelete = confirm('Are you sure?')
-    if (confirmDelete) {
-      const names = this.dataGrist.selected.map(record => record.name)
-      if (names && names.length > 0) {
-        const response = await client.query({
-          query: gql`
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(async result => {
+      if (result.value) {
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success')
+        const names = this.dataGrist.selected.map(record => record.name)
+        if (names && names.length > 0) {
+          const response = await client.query({
+            query: gql`
             mutation {
               deleteCompanies(${gqlBuilder.buildArgs({ names })})
             }
           `
-        })
+          })
 
-        if (!response.errors) {
-          this.dataGrist.fetch()
-          document.dispatchEvent(
-            new CustomEvent('notify', {
-              detail: {
-                message: i18next.t('text.data_deleted_successfully')
-              }
-            })
-          )
+          if (!response.errors) {
+            this.dataGrist.fetch()
+            document.dispatchEvent(
+              Swal.fire({
+                // position: 'top-end',
+                type: 'info',
+                title: 'Your work has been deleted',
+                showConfirmButton: false,
+                timer: 1500
+              })
+              // new CustomEvent('notify', {
+              //   detail: {
+              //     message: i18next.t('text.data_deleted_successfully')
+              //   }
+              // })
+            )
+          }
         }
       }
-    }
+    })
   }
 
   get _columns() {
