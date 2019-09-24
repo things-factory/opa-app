6fe39ea6f5883e51797aa95d40bf2802234decab
seoul-hatiolab <seoul@hatiolab.com>
2019-09-24 19:20:22 +0800

    Add inventory and transport summary

---
+++ client/pages/master/inventory-summary-report.js
@@ -0,0 +1,395 @@
+import { MultiColumnFormStyles } from '@things-factory/form-ui'
+import '@things-factory/form-ui'
+import '@things-factory/grist-ui'
+import { i18next, localize } from '@things-factory/i18n-base'
+import { client, gqlBuilder, isMobileDevice, navigate, PageView, ScrollbarStyles } from '@things-factory/shell'
+import gql from 'graphql-tag'
+import { css, html } from 'lit-element'
+import '../components/import-pop-up'
+import Swal from 'sweetalert2'
+
+class InventorySummaryReport extends localize(i18next)(PageView) {
+  static get properties() {
+    return {
+      _searchFields: Array,
+      config: Object,
+      data: Object,
+      importHandler: Object
+    }
+  }
+
+  static get styles() {
+    return [
+      ScrollbarStyles,
+      css`
+        :host {
+          display: flex;
+          flex-direction: column;
+
+          overflow: hidden;
+        }
+
+        search-form {
+          overflow: visible;
+        }
+        .grist {
+          display: flex;
+          flex-direction: column;
+          flex: 1;
+          overflow-y: auto;
+        }
+        data-grist {
+          overflow-y: hidden;
+          flex: 1;
+        }
+      `
+    ]
+  }
+
+  render() {
+    return html`
+      <search-form
+        id="search-form"
+        .fields=${this._searchFields}
+        initFocus="description"
+        @submit=${async () => this.dataGrist.fetch()}
+      ></search-form>
+
+      <div class="grist">
+        <data-grist
+          .mode=${isMobileDevice() ? 'LIST' : 'GRID'}
+          .config=${this.config}
+          .fetchHandler="${this.fetchHandler.bind(this)}"
+        ></data-grist>
+      </div>
+    `
+  }
+
+  get context() {
+    return {
+      title: i18next.t('title.product'),
+      actions: [
+        {
+          title: i18next.t('button.save'),
+          action: this._saveProducts.bind(this)
+        },
+        {
+          title: i18next.t('button.delete'),
+          action: this._deleteProducts.bind(this)
+        }
+      ],
+      exportable: {
+        name: i18next.t('title.product'),
+        data: this._exportableData.bind(this)
+      },
+      importable: {
+        handler: this._importableData.bind(this)
+      }
+    }
+  }
+
+  activated(active) {
+    if (JSON.parse(active) && this.dataGrist) {
+      this.dataGrist.fetch()
+    }
+  }
+
+  async firstUpdated() {
+    this._searchFields = [
+      {
+        label: i18next.t('label.name'),
+        name: 'name',
+        props: {
+          searchOper: 'like',
+          placeholder: i18next.t('label.name')
+        }
+      },
+      {
+        label: i18next.t('label.type'),
+        name: 'type',
+        props: {
+          searchOper: 'like',
+          placeholder: i18next.t('label.type')
+        }
+      }
+    ]
+
+    this.config = {
+      rows: {
+        selectable: {
+          multiple: true
+        }
+      },
+      columns: [
+        { type: 'gutter', gutterName: 'dirty' },
+        { type: 'gutter', gutterName: 'sequence' },
+        { type: 'gutter', gutterName: 'row-selector', multiple: true },
+        // {
+        //   type: 'gutter',
+        //   gutterName: 'button',
+        //   icon: 'reorder',
+        //   handlers: {
+        //     click: (columns, data, column, record, rowIndex) => {
+        //       if (record.id) navigate(`product_options/${record.id}`)
+        //     }
+        //   }
+        // },
+        {
+          type: 'string',
+          name: 'name',
+          record: {
+            editable: true
+          },
+          header: i18next.t('field.name'),
+          width: 180
+        },
+        {
+          type: 'string',
+          name: 'description',
+          record: {
+            editable: true
+          },
+          header: i18next.t('field.description'),
+          width: 250
+        },
+        {
+          type: 'string',
+          name: 'type',
+          record: {
+            align: 'center',
+            editable: true
+          },
+          header: i18next.t('field.type'),
+          width: 150
+        },
+        {
+          type: 'object',
+          name: 'updater',
+          record: {
+            align: 'center',
+            editable: false
+          },
+          header: i18next.t('field.updater'),
+          width: 250
+        },
+        {
+          type: 'datetime',
+          name: 'updatedAt',
+          record: {
+            align: 'center',
+            editable: false
+          },
+          header: i18next.t('field.updated_at'),
+          width: 180
+        }
+      ]
+    }
+  }
+
+  get searchForm() {
+    return this.shadowRoot.querySelector('search-form')
+  }
+
+  get dataGrist() {
+    return this.shadowRoot.querySelector('data-grist')
+  }
+
+  _importableData(records) {
+    setTimeout(() => {
+      openPopup(
+        html`
+          <import-pop-up
+            .records=${records}
+            .config=${this.config}
+            .importHandler="${this.importHandler.bind(this)}"
+          ></import-pop-up>
+        `,
+        {
+          backdrop: true,
+          size: 'large',
+          title: i18next.t('title.import')
+        }
+      )
+    }, 500)
+  }
+
+  async fetchHandler({ page, limit, sorters = [] }) {
+    const response = await client.query({
+      query: gql`
+        query {
+          products(${gqlBuilder.buildArgs({
+            filters: this._conditionParser(),
+            pagination: { page, limit },
+            sortings: sorters
+          })}) {
+            items {
+              id
+              name
+              description
+              type
+              updater {
+                id
+                name
+                description
+              }
+              updatedAt
+            }
+            total
+          }
+        }
+        `
+    })
+
+    if (!response.errors) {
+      return {
+        total: response.data.products.total || 0,
+        records: response.data.products.items || []
+      }
+    }
+  }
+
+  async importHandler(patches) {
+    const response = await client.query({
+      query: gql`
+          mutation {
+            updateMultipleProduct(${gqlBuilder.buildArgs({
+              patches
+            })}) {
+              name
+            }
+          }
+        `
+    })
+
+    if (!response.errors) {
+      history.back()
+      this.dataGrist.fetch()
+      document.dispatchEvent(
+        new CustomEvent('notify', {
+          detail: {
+            message: i18next.t('text.data_imported_successfully')
+          }
+        })
+      )
+    }
+  }
+
+  _conditionParser() {
+    return this.searchForm
+      .getFields()
+      .filter(field => (field.type !== 'checkbox' && field.value && field.value !== '') || field.type === 'checkbox')
+      .map(field => {
+        return {
+          name: field.name,
+          value:
+            field.type === 'text'
+              ? field.value
+              : field.type === 'checkbox'
+              ? field.checked
+              : field.type === 'number'
+              ? parseFloat(field.value)
+              : field.value,
+          operator: field.getAttribute('searchOper')
+        }
+      })
+  }
+
+  async _saveProducts() {
+    let patches = this.dataGrist.dirtyRecords
+    if (patches && patches.length) {
+      patches = patches.map(product => {
+        let patchField = product.id ? { id: product.id } : {}
+        const dirtyFields = product.__dirtyfields__
+        for (let key in dirtyFields) {
+          patchField[key] = dirtyFields[key].after
+        }
+        patchField.cuFlag = product.__dirty__
+
+        return patchField
+      })
+
+      const response = await client.query({
+        query: gql`
+            mutation {
+              updateMultipleProduct(${gqlBuilder.buildArgs({
+                patches
+              })}) {
+                name
+              }
+            }
+          `
+      })
+
+      if (!response.errors) {
+        this.dataGrist.fetch()
+        document.dispatchEvent(
+          new CustomEvent('notify', {
+            detail: {
+              message: i18next.t('text.data_updated_successfully')
+            }
+          })
+        )
+      }
+    }
+  }
+
+  async _deleteProducts() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(async result => {
+      if (result.value) {
+        const names = this.dataGrist.selected.map(record => record.name)
+        if (names && names.length > 0) {
+          const response = await client.query({
+            query: gql`
+            mutation {
+              deleteProducts(${gqlBuilder.buildArgs({ names })})
+            }
+          `
+          })
+
+          if (!response.errors) {
+            this.dataGrist.fetch()
+            document.dispatchEvent(
+              new CustomEvent('notify', {
+                detail: {
+                  message: i18next.t('text.data_updated_successfully')
+                }
+              })
+            )
+          }
+        }
+      }
+    })
+  }
+
+  get _columns() {
+    return this.config.columns
+  }
+
+  _exportableData() {
+    let records = []
+    if (this.dataGrist.selected && this.dataGrist.selected.length > 0) {
+      records = this.dataGrist.selected
+    } else {
+      records = this.dataGrist.data.records
+    }
+
+    return records.map(item => {
+      return this._columns
+        .filter(column => column.type !== 'gutter')
+        .reduce((record, column) => {
+          record[column.name] = item[column.name]
+          return record
+        }, {})
+    })
+  }
+}
+
+window.customElements.define('inventory-summary-report', InventorySummaryReport)
