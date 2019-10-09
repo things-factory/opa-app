11a8d15f282175f0d973a4021a1b17be80e4fc67
horwengliang <horwengliang@gmail.com>
2019-10-09 20:33:06 +0800

    update vas worksheet

--- client/pages/vas/vas-worksheet-list.js
+++ client/pages/vas/vas-worksheet-list.js
@@ -1,11 +1,13 @@
+import { getCodeByName } from '@things-factory/code-base'
+import '@things-factory/form-ui'
 import '@things-factory/grist-ui'
 import { i18next, localize } from '@things-factory/i18n-base'
 import { client, gqlBuilder, isMobileDevice, navigate, PageView, ScrollbarStyles } from '@things-factory/shell'
 import gql from 'graphql-tag'
 import { css, html } from 'lit-element'
-import { ORDER_STATUS } from '../constants/order'
+import { WORKSHEET_TYPE, WORKSHEET_STATUS } from '../inbound/constants/worksheet'
 
-class VasOrderList extends localize(i18next)(PageView) {
+class VasWorksheetList extends localize(i18next)(PageView) {
   static get styles() {
     return [
       ScrollbarStyles,
@@ -57,52 +59,48 @@ class VasOrderList extends localize(i18next)(PageView) {
 
   get context() {
     return {
-      title: i18next.t('title.vas_orders'),
+      title: i18next.t('title.vas_worksheets'),
       exportable: {
-        name: i18next.t('title.vas_orders'),
+        name: i18next.t('title.vas_worksheets'),
         data: this._exportableData.bind(this)
-      },
-      importable: {
-        handler: () => {}
       }
     }
   }
 
-  pageUpdated(changed, lifecycle) {
+  pageUpdated(changes, lifecycle) {
     if (this.active) {
       this.dataGrist.fetch()
     }
   }
 
-  pageInitialized() {
+  async pageInitialized() {
+    const _worksheetTypes = await getCodeByName('WORKSHEET_TYPES')
+    const _worksheetStatus = await getCodeByName('WORKSHEET_STATUS')
+
     this._searchFields = [
       {
-        label: i18next.t('field.vas_order_no'),
         name: 'name',
+        label: i18next.t('field.name'),
         type: 'text',
         props: { searchOper: 'like' }
       },
       {
-        label: i18next.t('field.status'),
         name: 'status',
+        label: i18next.t('label.status'),
         type: 'select',
         options: [
           { value: '' },
-          { name: i18next.t(`label.${ORDER_STATUS.PENDING.name}`), value: ORDER_STATUS.PENDING.value },
-          { name: i18next.t(`label.${ORDER_STATUS.PENDING_RECEIVE.name}`), value: ORDER_STATUS.PENDING_RECEIVE.value },
-          {
-            name: i18next.t(`label.${ORDER_STATUS.READY_TO_EXECUTE.name}`),
-            value: ORDER_STATUS.READY_TO_EXECUTE.value
-          },
-          { name: i18next.t(`label.${ORDER_STATUS.EXECUTING.name}`), value: ORDER_STATUS.EXECUTING.value },
-          { name: i18next.t(`label.${ORDER_STATUS.DONE.name}`), value: ORDER_STATUS.DONE.value }
+          ..._worksheetStatus.map(status => {
+            return { name: i18next.t(`label.${status.description}`), value: status.name }
+          })
         ],
         props: { searchOper: 'eq' }
       }
     ]
 
     this.config = {
-      rows: { selectable: { multiple: true } },
+      list: { fields: ['vasOrder', 'bizplace', 'status'] },
+      rows: { appendable: false },
       columns: [
         { type: 'gutter', gutterName: 'dirty' },
         { type: 'gutter', gutterName: 'sequence' },
@@ -112,22 +110,46 @@ class VasOrderList extends localize(i18next)(PageView) {
           icon: 'reorder',
           handlers: {
             click: (columns, data, column, record, rowIndex) => {
-              const status = record.status
-              if (status === ORDER_STATUS.REJECTED.value) {
-                navigate(`rejected_vas_order/${record.name}`) // 1. move to rejected detail page
-              } else {
-                navigate(`vas_order_detail/${record.name}`)
+              if (!record.id) return
+              const type = record.type
+              // Handle VAS
+              if (type === WORKSHEET_TYPE.VAS.value) {
+                navigate(`worksheet_vas/${record.name}`)
               }
             }
           }
         },
         {
-          type: 'string',
-          name: 'name',
-          header: i18next.t('field.vas_order_no'),
+          type: 'object',
+          name: 'vasOrder',
+          header: i18next.t('field.vas_order'),
           record: { align: 'center' },
           sortable: true,
+          width: 200
+        },
+        {
+          type: 'object',
+          name: 'bizplace',
+          header: i18next.t('field.customer'),
+          record: { align: 'center' },
+          sortable: true,
+          width: 200
+        },
+        {
+          type: 'string',
+          name: 'name',
+          header: i18next.t('field.name'),
+          record: { align: 'left' },
+          sortable: true,
           width: 180
+        },
+        {
+          type: 'string',
+          name: 'type',
+          header: i18next.t('field.type'),
+          record: { align: 'center' },
+          sortable: true,
+          width: 160
         },
         {
           type: 'string',
@@ -139,8 +161,16 @@ class VasOrderList extends localize(i18next)(PageView) {
         },
         {
           type: 'datetime',
-          name: 'createdAt',
-          header: i18next.t('field.created_at'),
+          name: 'startedAt',
+          header: i18next.t('field.started_at'),
+          record: { align: 'center' },
+          sortable: true,
+          width: 160
+        },
+        {
+          type: 'datetime',
+          name: 'endedAt',
+          header: i18next.t('field.ended_at'),
           record: { align: 'center' },
           sortable: true,
           width: 160
@@ -149,17 +179,17 @@ class VasOrderList extends localize(i18next)(PageView) {
           type: 'datetime',
           name: 'updatedAt',
           header: i18next.t('field.updated_at'),
-          record: { align: 'center' },
+          record: { editable: false, align: 'center' },
           sortable: true,
-          width: 160
+          width: 150
         },
         {
           type: 'object',
-          name: 'creator',
-          header: i18next.t('field.creator'),
-          record: { align: 'center' },
+          name: 'updater',
+          header: i18next.t('field.updater'),
+          record: { editable: false, align: 'center' },
           sortable: true,
-          width: 160
+          width: 150
         }
       ]
     }
@@ -173,27 +203,42 @@ class VasOrderList extends localize(i18next)(PageView) {
     return this.shadowRoot.querySelector('data-grist')
   }
 
-  get _columns() {
-    return this.config.columns
-  }
-
   async fetchHandler({ page, limit, sorters = [] }) {
+    const filters = this.searchForm.queryFilters
+    filters.push({
+      name: 'type',
+      operator: 'in',
+      value: [WORKSHEET_TYPE.VAS.value]
+    })
     const response = await client.query({
       query: gql`
         query {
-          vasOrders(${gqlBuilder.buildArgs({
-            filters: this.searchForm.queryFilters,
+          worksheets(${gqlBuilder.buildArgs({
+            filters,
             pagination: { page, limit },
             sortings: sorters
           })}) {
             items {
               id
-              name
-              status
-              createdAt
-              updatedAt
-              creator {
+              vasOrder {
+                id
                 name
+                description
+              }
+              bizplace {
+                id
+                name
+                description
+              }
+              name
+              type
+              status
+              startedAt
+              endedAt
+              updatedAt
+              updater {
+                name
+                description
               }
             }
             total
@@ -204,10 +249,14 @@ class VasOrderList extends localize(i18next)(PageView) {
 
     if (!response.errors) {
       return {
-        total: response.data.vasOrders.total || 0,
-        records: response.data.vasOrders.items || []
+        total: response.data.worksheets.total || 0,
+        records: response.data.worksheets.items || []
       }
     }
+  }
+
+  get _columns() {
+    return this.config.columns
   }
 
   _exportableData() {
@@ -229,4 +278,4 @@ class VasOrderList extends localize(i18next)(PageView) {
   }
 }
 
-window.customElements.define('vas-order-list', VasOrderList)
+window.customElements.define('vas-worksheet-list', VasWorksheetList)
