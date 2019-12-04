26f046948f710e451e4d8a912d71393bd1a09f24
leemyunggyu <jaylee@hatiolab.com>
2019-12-04 15:18:32 +0900

    Update opa-menus.ts

--- server/graphql/resolvers/opa-menu/opa-menus.ts
+++ server/graphql/resolvers/opa-menu/opa-menus.ts
@@ -3,75 +3,78 @@ import { Menu } from '@things-factory/menu-base'
 import { Equal, getRepository, In, IsNull } from 'typeorm'
 
 export const opaMenusResolver = {
-  async opaMenus(_: any, params: any, context: any) {
-    const response = await getRepository(Menu).query(`
-      SELECT
-        category
-      FROM
-        priviledges
-      WHERE id IN (
+  async opaMenus(_: any, _params: any, context: any) {
+    let menus = []
+    if (context && context.state && context.state.domain && context.state.domain.id) {
+      const response = await getRepository(Menu).query(`
         SELECT
-          DISTINCT(priviledges_id)
+          category
         FROM
-          roles_priviledges
-        WHERE
-          roles_id IN (
-            SELECT
-              roles_id
-            FROM
-              users_roles
-            WHERE
-              users_id = '${context.state.user.id}'
-          )
-      ) AND NAME = 'role'
-      AND domain_id = '${context.state.domain.id}'
+          priviledges
+        WHERE id IN (
+          SELECT
+            DISTINCT(priviledges_id)
+          FROM
+            roles_priviledges
+          WHERE
+            roles_id IN (
+              SELECT
+                roles_id
+              FROM
+                users_roles
+              WHERE
+                users_id = '${context.state.user.id}'
+            )
+        ) AND NAME = 'role'
+        AND domain_id = '${context.state.domain.id}'
     `)
-    const userRoles = response.map((priviledge: Priviledge) => priviledge.category)
+      const userRoles = response.map((priviledge: Priviledge) => priviledge.category)
 
-    if (userRoles.length == 0) {
-      return
+      if (userRoles.length) {
+        menus = await getRepository(Menu).find({
+          where: [
+            {
+              hiddenFlag: false,
+              menuType: Equal('MENU'),
+              domain: context.state.domain,
+              category: In(userRoles)
+            },
+            {
+              hiddenFlag: false,
+              menuType: Equal('MENU'),
+              domain: context.state.domain,
+              category: IsNull()
+            },
+            {
+              hiddenFlag: false,
+              menuType: Equal('MENU'),
+              domain: context.state.domain,
+              category: ''
+            }
+          ],
+          order: {
+            rank: 'ASC'
+          },
+          relations: ['domain', 'parent', 'childrens']
+        })
+
+        menus = menus
+          .map((groupMenu: Menu) => {
+            return {
+              ...groupMenu,
+              childrens: groupMenu.childrens
+                .filter(
+                  (subMenu: Menu) =>
+                    (!subMenu.hiddenFlag && userRoles.includes(subMenu.category)) ||
+                    (!subMenu.hiddenFlag && !subMenu.category)
+                )
+                .sort((a: Menu, b: Menu) => a.rank - b.rank)
+            }
+          })
+          .filter((groupMenu: Menu) => groupMenu.childrens && groupMenu.childrens.length)
+      }
     }
 
-    const menus = await getRepository(Menu).find({
-      where: [
-        {
-          hiddenFlag: false,
-          menuType: Equal('MENU'),
-          domain: context.state.domain,
-          category: In(userRoles)
-        },
-        {
-          hiddenFlag: false,
-          menuType: Equal('MENU'),
-          domain: context.state.domain,
-          category: IsNull()
-        },
-        {
-          hiddenFlag: false,
-          menuType: Equal('MENU'),
-          domain: context.state.domain,
-          category: ''
-        }
-      ],
-      order: {
-        rank: 'ASC'
-      },
-      relations: ['domain', 'parent', 'childrens']
-    })
-
     return menus
-      .map((groupMenu: Menu) => {
-        return {
-          ...groupMenu,
-          childrens: groupMenu.childrens
-            .filter(
-              (subMenu: Menu) =>
-                (!subMenu.hiddenFlag && userRoles.includes(subMenu.category)) ||
-                (!subMenu.hiddenFlag && !subMenu.category)
-            )
-            .sort((a: Menu, b: Menu) => a.rank - b.rank)
-        }
-      })
-      .filter((groupMenu: Menu) => groupMenu.childrens && groupMenu.childrens.length)
   }
 }
