9cb8b342aaf0cc6366b8a98bca7b93d77f35ac19
farishelmi <farishelmi@hatiolab.com>
2019-12-16 18:44:09 +0800

    update translation

--- client/pages/order/goods-received-note/upload-received-note.js
+++ client/pages/order/goods-received-note/upload-received-note.js
@@ -101,17 +101,11 @@ class UploadReceivedNote extends localize(i18next)(LitElement) {
     this._loadedFlag = {}
   }
 
-  get context() {
-    return {
-      title: i18next.t('title.create_goods_received_note')
-    }
-  }
-
   render() {
     return html`
       <form class="multi-column-form">
         <fieldset>
-          <legend>${i18next.t('title.upload_goods_received_note')}</legend>
+          <legend>${i18next.t('title.upload_grn')}</legend>
           <file-uploader custom-input id="grnUpload" name="attachments"></file-uploader>
         </fieldset>
       </form>
