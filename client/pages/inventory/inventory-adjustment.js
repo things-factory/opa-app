9fdaa2fe6a26eac51b88dd1b54275371609ece59
ChrisLim <chrislim@hatiolab.com>
2020-02-14 12:16:17 +0800

    Update inventory-adjustment.js

--- client/pages/inventory/inventory-adjustment.js
+++ client/pages/inventory/inventory-adjustment.js
@@ -286,7 +286,7 @@ class InventoryAdjustment extends connect(store)(localize(i18next)(PageView)) {
     this._searchFields = [
       {
         label: i18next.t('field.customer'),
-        name: 'bizplace',
+        name: 'bizplace_id',
         type: 'select',
         options: [
           { value: '' },
