16e515553fb062c9eb9068fe2c6117b9d7ac346f
horwengliang <horwengliang@gmail.com>
2020-03-20 14:25:24 +0800

    Revert "Revert "switch translation from constant to page""
    This reverts commit b61492763108663fc450a6463fe314f8c1541261.

--- client/pages/order/constants/picking-standard.js
+++ client/pages/order/constants/picking-standard.js
@@ -1,12 +1,10 @@
-import { i18next } from '@things-factory/i18n-base'
-
 export const PICKING_STANDARD = {
   SELECT_BY_PALLET: {
-    name: i18next.t('label.select_by_pallet'),
+    name: 'label.select_by_pallet',
     value: 'SELECT_BY_PALLET'
   },
   SELECT_BY_PRODUCT: {
-    name: i18next.t('label.select_by_product'),
+    name: 'label.select_by_product',
     value: 'SELECT_BY_PRODUCT'
   }
 }
