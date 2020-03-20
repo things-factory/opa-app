093accf2161918a4b6eb42d55f416e335110d7ab
horwengliang <horwengliang@gmail.com>
2020-03-20 14:25:00 +0800

    Revert "Revert "multiple picking standard updated""
    This reverts commit 74c043543883801a92c152ce968d347a52d272c5.

---
+++ client/pages/order/constants/picking-standard.js
@@ -0,0 +1,12 @@
+import { i18next } from '@things-factory/i18n-base'
+
+export const PICKING_STANDARD = {
+  PICK_BY_PALLET: {
+    name: i18next.t('label.pick_by_pallet'),
+    value: 'PICK_BY_PALLET'
+  },
+  PICK_BY_PRODUCT: {
+    name: i18next.t('label.pick_by_product'),
+    value: 'PICK_BY_PRODUCT'
+  }
+}
